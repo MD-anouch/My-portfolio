58f8fa83e48dae55da7867fbd8c636241de2f380
MD-anouch <mou-998@hotmail.fr>
2020-02-05 15:06:59 +0100

    first commit

---
+++ src/components/contact.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
+
+
+class Contact extends Component {
+  render() {
+    return(
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>Mouad Anouch</h2>
+            <img
+              src="https://www.garvisor.com/frontend/images/empty-user-pic.png"
+              alt="avatar"
+              style={{height: '250px'}}
+               />
+             <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+
+          </Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr/>
+
+            <div className="contact-list">
+              <List>
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-phone-square" aria-hidden="true"/>
+                    06 87 65 40 51
+                  </ListItemContent>
+                </ListItem>
+
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                  <i class="fa fa-linkedin" aria-hidden="true"/>
+                    Mouad Anouch
+                  </ListItemContent>
+                </ListItem>
+
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-envelope" aria-hidden="true"/>
+                    mou-998@hotmail.fr
+                  </ListItemContent>
+                </ListItem>
+
+                {/* <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-skype" aria-hidden="true"/>
+                    MySkypeID
+                  </ListItemContent>
+                </ListItem> */}
+
+
+              </List>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
+    )
+  }
+}
+
+export default Contact;
---
+++ src/components/resume.js
@@ -0,0 +1,116 @@
+import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+import Education from './education';
+import Experience from './experience';
+import Skills from './skills';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+
+class Resume extends Component {
+  render() {
+    return(
+      <div>
+        <Grid>
+          <Cell col={4}>
+            <div style={{textAlign: 'center'}}>
+              <img
+                src="https://www.garvisor.com/frontend/images/empty-user-pic.png"
+                alt="avatar"
+                style={{height: '200px'}}
+                 />
+            </div>
+
+            <h2 style={{paddingTop: '2em'}}>Mouad Anouch</h2>
+            <h4 style={{color: 'grey'}}>Programmer</h4>
+            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+            <h5>Address</h5>
+            <p>Bd sebta Residence Al Alia G8 APP8, Mohammedia </p>
+            <h5>Phone</h5>
+            <p>06.87.65.40.51</p>
+            <h5>Email</h5>
+            {/* <p>mou-998@hotmail.fr</p> */}
+            <a className='mail' href='mailto:mou-998@hotmail.fr'>mou-998@hotmail.fr</a>
+            <h5>Web</h5>
+            <p>mywebsite.com</p>
+            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+            {/* <FontAwesomeIcon icon="download" /> */}
+          
+            <a href='https://drive.google.com/file/d/1U9fA-6-eJxt8jaAE5XgxZ5NkuCRkZ07w/view' target="_blank" rel="noopener noreferrer">  <i className="fa fa-download"/> Download my cv</a>
+          </Cell>
+          <Cell className="resume-right-col" col={8}>
+            <h2>Education</h2>
+
+            <Education
+                 startYear={2015}
+                 endYear={2016}
+                 schoolName="LYCCEE TECHNIQUE MOHAMMEDIA"
+                 schoolDescription="Baccalauréat System électrique"
+                  />
+            <Education
+              startYear={2016}
+              endYear={2018}
+              schoolName="Bts Khawarizmi"
+              schoolDescription="Études brevet de technicien supérieur "
+               />
+
+               <Education
+                 startYear={2018}
+                 endYear={2019}
+                 schoolName="IFIAG"
+                 schoolDescription="Licence Professionnel Management et Entreprenariat"
+                  />   
+              <Education
+                 startYear={2019}
+                 endYear={2019}
+                 schoolName="3W academy"
+                 schoolDescription="Formation Développeur WEB Full-Stack"
+                  />   
+              
+                <hr style={{borderTop: '3px solid #e22947'}} />
+
+              <h2>Experience</h2>
+
+            <Experience
+              startYear={'01/08/2017'}
+              endYear={'30/08/2017'}
+              jobName="CONCEPTEC"
+              jobDescription="Stage D’observation"
+              />
+
+              <Experience
+                startYear={'03/05/2018'}
+                endYear={'06/06/2018'}
+                jobName="SOMAC"
+                jobDescription="Stage"
+                />
+              <hr style={{borderTop: '3px solid #e22947'}} />
+              <h2>Skills</h2>
+              <Skills
+                skill="javascript"
+                progress={100}
+                />
+                <Skills
+                  skill="HTML/CSS"
+                  progress={80}
+                  />
+                  <Skills
+                    skill="laravel"
+                    progress={60}
+                    />
+                    <Skills
+                      skill="React"
+                      progress={25}
+                      />
+
+
+          </Cell>
+        </Grid>
+      </div>
+    )
+  }
+}
+
+export default Resume;
