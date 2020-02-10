9b7ee0cb15e3e8e9b2b2e9231970b525101db3fa
MD-anouch <mou-998@hotmail.fr>
2020-02-10 21:35:20 +0100

    form v3

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,59 +1,24 @@
 import React, { Component } from 'react'
 
-const encode = (data) => {
-    return Object.keys(data)
-        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-        .join("&");
-  }
-
-  export default class Form extends Component {
-    constructor(props) {
-      super(props);
-      this.state = { name: "", email: "", message: "" };
-    }
-
-    /* Here’s the juicy bit for posting the form submission */
-
-    handleSubmit = e => {
-      fetch("/", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: encode({ "form-name": "contact", ...this.state })
-      })
-        .then(() => alert("Success!"))
-        .catch(error => alert(error));
-
-      e.preventDefault();
-    };
-
-    handleChange = e => this.setState({ [e.target.name]: e.target.value });
-
+export default class Form extends Component {
     render() {
-      const { name, email, message } = this.state;
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <label>
-              Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message: <textarea name="message" value={message} onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
-      );
+        return (
+            <div>
+                <form name="contact" method="post" netlify="true">
+                    <p>
+                        <label>Your Name: <input type="text" name="name"/></label>
+                    </p>
+                    <p>
+                        <label>Your Email: <input type="email" name="email"/></label>
+                    </p>
+                    <p>
+                        <label>Message: <textarea name="message"></textarea></label>
+                    </p>
+                    <p>
+                        <button type="submit">Send</button>
+                    </p>
+                </form>
+            </div>
+        )
     }
-  }
-
-
-  
+}
