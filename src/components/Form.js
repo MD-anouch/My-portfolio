5e84853f15c679ed99686e6b7ba1aef04220ac02
MD-anouch <mou-998@hotmail.fr>
2020-02-10 21:41:38 +0100

    form change

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,20 +4,20 @@ export default class Form extends Component {
     render() {
         return (
             <div>
-                <form name="contact" method="post" netlify="true">
-                    <p>
-                        <label>Your Name: <input type="text" name="name"/></label>
-                    </p>
-                    <p>
-                        <label>Your Email: <input type="email" name="email"/></label>
-                    </p>
-                    <p>
-                        <label>Message: <textarea name="message"></textarea></label>
-                    </p>
-                    <p>
-                        <button type="submit">Send</button>
-                    </p>
-                </form>
+                  <form name="contact" method="post" netlify="true">
+          <p>
+            <label>Your Name: <input type="text" name="name"/></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email"/></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
             </div>
         )
     }
